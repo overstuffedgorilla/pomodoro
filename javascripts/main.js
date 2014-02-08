1e73869f10dc6572166da11c00d7deb1a21d95dc
Marta Kostova <mkostova@camplight.net>
2014-02-08 22:33:45 +0200

    remove gif and change src of the landing image dynamically

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+var images=["pomodoro_screenshot_1_menu.jpg",
+  "pomodoro_screenshot_2_main_options.jpg",
+  "pomodoro_screenshot_3_notify_options.jpg",
+  "pomodoro_screenshot_4_voice_settings.jpg",
+  "pomodoro_screenshot_5_calendar_integration.jpg",
+  "pomodoro_screenshot_6_scripts.jpg",
+  "pomodoro_screenshot_7_global_shortcuts.jpg",
+  "pomodoro_screenshot_8_general_settings.jpg"]
+$(function(){
+  var i=0;
+  var loopImages = function() {
+    i += 1
+    if(i>=images.length)
+      i=0
+    document.getElementById("landing_image").setAttribute("src","images/"+images[i])
+  }
+  setInterval(loopImages, 2000);
+})

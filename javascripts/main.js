13f3e5e1fc5212d441c68c672bfc8d15808bf80f
Marta Kostova <mkostova@camplight.net>
2015-01-18 22:46:42 +0200

    change screenshots for 2.0.4

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,14 @@
-var images=["0_Timer_personal_time_manager_menu.png",
-  "1_Timer_properties.png",
-  "2_Timer_calendar_integration.png",
-  "3_Timer_applescript_integration.png",
-  "8_Timer_statistics.png"]
+var images=["pomodoro_timer_00_help.jpg",
+  "pomodoro_timer_1_menu.jpg",
+  "pomodoro_timer_2_main_options.jpg",
+  "pomodoro_timer_3_notify_options.jpg",
+  "pomodoro_timer_4_voice_settings.jpg",
+  "pomodoro_timer_5_calendar_integration.jpg",
+  "pomodoro_timer_6_scripts.jpg",
+  "pomodoro_timer_7_global_shortcuts.jpg",
+  "pomodoro_timer_8_general_settings.jpg",
+  "pomodoro_timer_9_statistics.jpg",
+  "pomodoro_timer_10_about.jpg"]
 $(function(){
   var i=0;
   var loopImages = function() {
